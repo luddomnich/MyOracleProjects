29695b719ce53cb0452ec6e1b495e0d6798b63c0
luddomnich <luddch@gmail.com>
2015-12-14 15:43:40 +0200

    quicksort v 1

---
+++ QuickSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = { 1, 3, 6, 7, 9, 50, 9 };
+			int start = array[0];
+			int end = array.Length - 1;
+
+			Console.WriteLine("Befofe sort: ");
+			print(array);
+			QuickSort.quicksort (array , start, end);
+			Console.WriteLine("After sorting: ");
+			print(array);
+			Console.ReadLine();
+			
+		
+		}
+
+		public static void print(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.Write(array[i] + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortProgram
+{
+	class QuickSort
+	{
+		 static int partition(int[] array, int start, int end)
+		{
+			int marker = start;
+			for (int i = start; i <= end; i++)
+			{
+				if (array[i] <= array[end])
+				{
+					int temp = array[marker]; 
+					array[marker] = array[i];
+					array[i] = temp;
+					marker += 1;
+				}
+			}
+			return marker - 1;
+		}
+
+		public static void quicksort(int[] array, int start, int end)
+		{
+			if (start < end)
+			{
+				int rod = partition(array, start, end);
+				quicksort(array, start, rod - 1);
+				quicksort(array, rod + 1, end);
+			}
+		}
+		
+	}
+}
